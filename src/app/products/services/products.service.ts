af0f931e9240e5649b0018921502231fb0dcb304
TitoCalleros <mirna.molina@gmail.com>
2025-04-09 19:27:55 -0600

    Se usa la varaible definida en el environment.
    Se crea una interface de las opciones del get de los productos, que incluye el limit, offset y gender.

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -2,6 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ProductsResponse } from '@products/interfaces/product.interface';
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+interface ProductOptions {
+  limit?: number;
+  offset?: number;
+  gender?: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +19,22 @@ export class ProductsService {
 
   private http = inject(HttpClient);
 
-  getProducts(): Observable<ProductsResponse> {
-    return this.http.get<ProductsResponse>('http://localhost:3000/api/products').pipe(
-      tap(resp => console.log(resp))
-    );
+
+
+  getProducts(options: ProductOptions): Observable<ProductsResponse> {
+
+    const { limit = 9, offset = 0, gender = '' } = options;
+
+    return this.http.get<ProductsResponse>(`${baseUrl}/products`, {
+      params: {
+        limit,
+        offset,
+        gender
+      }
+    })
+      .pipe(
+        tap(resp => console.log(resp))
+      );
   }
 
 }
