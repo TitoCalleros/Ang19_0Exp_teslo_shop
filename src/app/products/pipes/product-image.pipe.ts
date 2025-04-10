e17e33ddb504992685b56d998629edb95a9fecdf
TitoCalleros <mirna.molina@gmail.com>
2025-04-09 21:00:15 -0600

    Código del pipe

---
+++ src/app/products/pipes/product-image.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+const placeholder = './assets/images/no-image.jpg';
+
+@Pipe({
+  name: 'productImage'
+})
+
+export class ProductImagePipe implements PipeTransform {
+  transform(value: string | string[]): string {
+
+    if (!value) return placeholder;
+
+    if (typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
+    }
+
+    const image = value.at(0);
+
+    if (!image) return placeholder;
+
+    return `${baseUrl}/files/product/${image}`;
+  }
+}
