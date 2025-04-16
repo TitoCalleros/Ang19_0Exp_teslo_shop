84431f71c58fb5216d9f6eff9a5984685d94dc00
TitoCalleros <mirna.molina@gmail.com>
2025-04-15 21:22:14 -0600

    Implementación del PaginationService en home-page y gender-page

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { rxResource, toSignal } from '@angular/core/rxjs-interop'
-import { ActivatedRoute } from '@angular/router';
 
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
 import { PaginationComponent } from "@shared/components/pagination/pagination.component";
-import { map } from 'rxjs';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -15,20 +14,22 @@ import { map } from 'rxjs';
 export class HomePageComponent {
   productsService = inject(ProductsService);
 
-  activatedRoute = inject(ActivatedRoute);
+  paginationService = inject(PaginationService);
 
-  currentPage = toSignal(
-    this.activatedRoute.queryParamMap.pipe(
-      map( (params) => (params.get('page') ? +params.get('page')! : 1 )),
-      map( page => (isNaN(page) ? 1 : page) )
-    ),
-    {
-      initialValue: 1,
-    }
-  );
+  // activatedRoute = inject(ActivatedRoute);
+
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map( (params) => (params.get('page') ? +params.get('page')! : 1 )),
+  //     map( page => (isNaN(page) ? 1 : page) )
+  //   ),
+  //   {
+  //     initialValue: 1,
+  //   }
+  // );
 
   productResource = rxResource({
-    request: () => ({ page: this.currentPage() - 1 }),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({request}) => {
       return this.productsService.getProducts({limit: 9, offset: request.page * 9});
     }
