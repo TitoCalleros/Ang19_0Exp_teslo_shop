9cd3544392fbe4448a9b7a123baf88f1a7f179e5
TitoCalleros <mirna.molina@gmail.com>
2025-04-15 21:22:38 -0600

    Implementación del pagination-service en gender-page

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -6,16 +6,19 @@ import { map } from 'rxjs';
 
 import { ProductsService } from '@products/services/products.service';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { PaginationComponent } from "@shared/components/pagination/pagination.component";
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent, I18nSelectPipe],
+  imports: [ProductCardComponent, I18nSelectPipe, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
 
   route = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   gender = toSignal(
     this.route.params.pipe(
@@ -32,9 +35,12 @@ export class GenderPageComponent {
 
 
   productResource = rxResource({
-    request: () => ({gender: this.gender()}),
+
+    request: () => ({gender: this.gender(), page: this.paginationService.currentPage() - 1}),
     loader: ({request}) => {
       return this.productsService.getProducts({
+        limit: 9,
+        offset: request.page * 9,
         gender: request.gender,
       });
     }
