281599be26ff20ea32a30ae5c5a17b72edf9dd5c
TitoCalleros <mirna.molina@gmail.com>
2025-04-18 15:45:30 -0600

    Implementación de la función login en el servicio. Se establecen señales de user, token y authStatus.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal, computed } from '@angular/core';
 
+import { AuthResponse } from '@auth/interfaces/auth-response.interface';
 import { User } from '@auth/interfaces/user.interface';
+import { tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated'
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+const baseUrl = environment.baseUrl;
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -26,4 +31,18 @@ export class AuthService {
   user = computed<User|null>(() => this._user());
 
   token = computed<string|null>(() => this._token());
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponse>(`${baseUrl}/auth/login`, {
+      email,
+      password
+    }).pipe(
+      tap((response) => {
+        this._user.set(response.user);
+        this._token.set(response.token);
+        this._authStatus.set('authenticated');
+        localStorage.setItem('token', response.token);
+      })
+    )
+  }
 }
