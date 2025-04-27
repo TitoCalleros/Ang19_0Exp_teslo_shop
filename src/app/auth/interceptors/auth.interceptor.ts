839ecb66582ebf0064647cff58656c6cd3fb708b
TitoCalleros <mirna.molina@gmail.com>
2025-04-26 20:59:41 -0600

    Creación del authInterceptor para agregar el token a las peiticiones que vayan a la URL de check-status.

---
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpHandlerFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AuthService } from "@auth/services/auth.service";
+
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+
+  // Validación de sí el url del request apunta a la URL requerida de check-status. Si se desea verificar múltiples elementos, es mejor crear una función con los endpoints requeridos.
+  if (!req.url.includes('check-status')) return next(req);
+
+  const token = inject(AuthService).token()!;
+
+  // Clone the request to add the Authorization header.
+  const newReq = req.clone({
+    headers: req.headers.append('Authorization', `Bearer ${token}`),
+  });
+  return next(newReq);
+}
