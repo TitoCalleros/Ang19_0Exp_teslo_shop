055857fecbdaee913ed2644d1a954099f1fb9d4b
TitoCalleros <mirna.molina@gmail.com>
2025-04-27 00:17:30 -0600

    Creación de la pagina de registro.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-page.component.html',
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+  authService = inject(AuthService);
+  fb = inject(FormBuilder);
+  router = inject(Router);
+
+  hasError = signal(false);
+
+  registerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    fullName: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if ( this.registerForm.invalid ) {
+      this.setHasError();
+      return;
+    }
+
+    const {email = '', password = '', fullName = ''} = this.registerForm.value;
+
+    this.authService.register(email!, password!, fullName!).subscribe((isValid) => {
+      if (isValid) {
+        this.router.navigateByUrl('/');
+      }
+
+      this.setHasError();
+    })
+  }
+
+  setHasError() {
+    this.hasError.set(true);
+    setTimeout(() => {
+      this.hasError.set(false);
+    }, 2000);
+  }
+}
