7a80435d24e172e362609b9fd9317dd652c7b9d6
TitoCalleros <mirna.molina@gmail.com>
2025-05-02 15:22:50 -0600

    Creación de la estructura base del product-admin-page para mostrar un spinner y si hay un error al obtener el producto, sacar de la página al usuario.

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { ProductsService } from '@products/services/products.service';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-product-admin-page',
   imports: [],
   templateUrl: './product-admin-page.component.html',
 })
-export class ProductAdminPageComponent { }
+export class ProductAdminPageComponent {
+
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  productId = toSignal(this.activatedRoute.params.pipe(
+    map( params => params['id'] ?? '')
+  ));
+
+  productsService = inject(ProductsService);
+
+  productResource = rxResource({
+    request: () => ({id: this.productId() }),
+    loader: ({request}) => this.productsService.getProductById(request.id)
+  });
+
+  redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this.router.navigateByUrl('/admin/products');
+    }
+  })
+
+}
