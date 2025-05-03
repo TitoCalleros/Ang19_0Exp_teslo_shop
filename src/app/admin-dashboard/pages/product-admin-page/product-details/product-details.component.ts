f513eecf6f802feb551f023f0f95cfde69a7d424
TitoCalleros <mirna.molina@gmail.com>
2025-05-02 23:47:42 -0600

    Especificación y conexión de formulario al html.
    Se agrega el método onSubmit y el setFormValue.

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,13 +1,46 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { Product, Size } from '@products/interfaces/product.interface';
 import { ProductCarrouselComponent } from "@products/components/product-carrousel/product-carrousel.component";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@shared/utils/form-utils';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarrouselComponent],
+  imports: [ProductCarrouselComponent, ReactiveFormsModule],
   templateUrl: './product-details.component.html',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
   product = input.required<Product>();
-  sizes = input<Size[]>();
+
+  fb = inject(FormBuilder);
+
+  productDetailsForm = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(10)]],
+    description: ['', [Validators.required]],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0,  [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+  });
+
+  sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    this.productDetailsForm.reset(formLike as any);
+    this.productDetailsForm.patchValue({tags: formLike.tags?.join(',')});
+  }
+
+  onSubmit() {
+    console.log(this.productDetailsForm.value);
+
+  }
  }
